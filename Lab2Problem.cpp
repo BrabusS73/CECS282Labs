1163fdff3c66a9a97e7000bd40ec17c7f7216943
BrabusS73 <48600478+BrabusS73@users.noreply.github.com>
2020-09-01 18:18:31 -0700

    Create Lab2Problem.cpp

---
+++ Lab2Problem.cpp
@@ -0,0 +1,66 @@
+//Nhan Vo and Ian Lee
+//CECS 282 LAB 2 - Problem 2
+#include <iostream>
+using namespace std;
+
+class Account {
+    float balance;
+
+    public:
+    Account() {
+        balance = 0;
+    }
+
+    Account(int balance) {
+        this -> balance = balance;
+    }
+
+    int deposit(int amount) {
+        balance += amount;
+        return 0;
+    }
+
+    int withdraw(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            return 0;
+        }
+
+        else if ((amount > balance) && (balance > 5)) {
+            balance -= 5;
+            return 0; 
+        }
+        
+        return 0;
+    }
+
+    float get_balance() {
+        return balance;
+    }
+
+    int add_interest(float rate) {
+        balance *= (1 + rate / 100);
+        return 0;
+    }
+};
+
+int main() {
+    int init_amount;
+    float rate;
+    int months = 0;
+    cout << "Enter initial amount of your account:";
+    cin >> init_amount;
+    cout << "Enter your interest rate:";
+    cin >> rate;
+    Account my_account(init_amount);
+    int final_amount = init_amount * 2;
+    while (my_account.get_balance() < final_amount) {
+        my_account.add_interest(rate / 12);
+        months++;
+    }
+
+    cout << "It took " << months << " for $" << init_amount << " to double at " << rate << "% annual interest." << "\n"; 
+    cout << "Account balance: $" << my_account.get_balance() << "\n";
+    return 0;
+
+}
